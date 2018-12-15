b300bd60e4fafcedc5c90e0d713eac76d370d6cb
taylorjohannsen <44182384+taylorjohannsen@users.noreply.github.com>
2018-12-14 21:51:49 -0800

    added a class that creates to-do list object

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-class toDo {
-    constructor(title, desription, date, priority) {
-        this.title = title;
-        this.desription = desription;
-        this.date = date;
-        this.priority = priority;
-    }
-}
+let userID = 0;
+let userInput;
 
 class projectRender {
-    constructor(projectName) {
-        this.projectName = projectName;
+    constructor(title, date, id) {
+        this.title = title;
+        this.date = date;
+        this.id = id;
     }
-
     renderName() {
-        document.getElementById('projName').innerHTML = this.projectName;
-        let elenName = document.getElementById('projList');
-        let createElement = document.createElement('div');
-        createElement.innerText = this.projectName;
-        elenName.appendChild(createElement);
-    }
-    deleteName() {
-        elenName.removeChild(createElement);
-    }
+        let idElem = document.createElement('div');
+
+        let titleElem = document.createElement('div');
+        titleElem.textContent = this.title;
+        let dateElem = document.createElement('div');
+        dateElem.textContent = this.date;
+
+        let testDelete = document.createElement('button');
+        testDelete.innerHTML = "Delete Project";
+        testDelete.addEventListener ("click", function() {
+            createElement.removeChild(document.getElementById(idElem.id));
+        })
+
+        let testEdit = document.createElement('button');
+        testEdit.innerHTML = "EDIT";
+        testEdit.addEventListener ("click", function() {
+            createElement.removeChild(document.getElementById(idElem.id));
+        })
+
+        idElem.id = this.id;
+        idElem.appendChild(titleElem);
+        idElem.appendChild(dateElem);
+        idElem.appendChild(testDelete);
+        idElem.appendChild(testEdit);
+
+        let createElement = document.getElementById('projList');
+        createElement.appendChild(idElem);
+        document.getElementById('title').value = '';
+        document.getElementById('date').value = '';    
+    }    
 }
 
+function render() {
+    let userTitle = document.getElementById('title').value;
+    let userDate = document.getElementById('date').value;
+    userInput = new projectRender(userTitle, userDate, userID);
+    userInput.renderName();
+    userID++;
+}
